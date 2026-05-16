40eb7b19ff327b78c4c2d97a8a9a726de22f1a61
Gent Myftiu <myftiug@gmail.com>
2026-05-16 21:47:48 +0200

    Test User Seeder

---
+++ database/seeders/MainUserSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class MainUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'first_name' => 'Nataliya',
+            'last_name' => 'Tarasyuk',
+            'email' => 'test@hsp-connect.com',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
