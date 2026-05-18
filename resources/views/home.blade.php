aead61bdeff0bfd86ff64336ec7ea85ae39fa4dc
Gent Myftiu <myftiug@gmail.com>
2026-05-18 21:08:51 +0200

    Registration - Email verification is missing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,7 +23,8 @@
                     <div class="c-srch-wrap">
                         <input class="c-srch-inp" id="compose-srch" type="text"
                                placeholder="Was besch&#xE4;ftigt dich gerade?"
-                               oninput="onComposeSrch()">
+                               oninput="onComposeSrch()"
+                               autocomplete="off">
                         <button class="c-srch-x" id="c-srch-x" onclick="clearComposeSrch()" style="display:none">
                             &#xD7;
                         </button>
