f9f29ab8228314742bf9547b4ac5d1eeaf376cc2
Gent Myftiu <myftiug@gmail.com>
2026-05-19 21:29:43 +0200

    Filter through Search Field (Block Welcome)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,8 +23,11 @@
                     <div class="c-srch-wrap">
                         <input type="text" name="fake-user" style="display:none" tabindex="-1" aria-hidden="true">
                         <input type="password" name="fake-pass" style="display:none" tabindex="-1" aria-hidden="true">
+                        <svg class="c-srch-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/>
+                        </svg>
                         <input class="c-srch-inp" id="compose-srch" type="text"
-                               placeholder="Was besch&#xE4;ftigt dich gerade?"
+                               placeholder="Beitr&#xE4;ge suchen&#x2026;"
                                oninput="onComposeSrch()"
                                autocomplete="off">
                         <button class="c-srch-x" id="c-srch-x" onclick="clearComposeSrch()" style="display:none">
