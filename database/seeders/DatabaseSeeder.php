b5e5603aa7a9f27db3ff28a8def6159e9665a228
Gent Myftiu <myftiug@gmail.com>
2026-05-19 22:07:59 +0200

    Feedback

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             TagsSeeder::class,
             MainUserSeeder::class,
             PostSeeder::class,
+            FaqItemSeeder::class,
         ]);
     }
 }
