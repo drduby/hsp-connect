90054b876dcbad36dcd90383954f0b3e3ba932c3
Gent Myftiu <myftiug@gmail.com>
2026-05-27 21:50:24 +0200

    Bugfix

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -24,6 +24,7 @@ class SecurityHeaders
         $response->headers->set('X-Frame-Options', 'SAMEORIGIN');
         $response->headers->set('Referrer-Policy', 'strict-origin-when-cross-origin');
         $response->headers->set('Permissions-Policy', 'camera=(), microphone=(), geolocation=()');
+        $response->headers->set('Cross-Origin-Opener-Policy', 'same-origin');
         $response->headers->set('Content-Security-Policy', $this->contentSecurityPolicy());
 
         if ($request->isSecure()) {
