4c2fb989b73ffacc2583c0dbf9601c5786511658
Gent Myftiu <myftiug@gmail.com>
2026-05-29 19:26:46 +0200

    Multilanguage

--- database/seeders/TagsSeeder.php
+++ database/seeders/TagsSeeder.php
@@ -14,21 +14,22 @@ class TagsSeeder extends Seeder
     public function run(): void
     {
         collect([
-            'Spastik' => '#0a6e7a',
-            'Muskeln' => '#b8762a',
-            'Entspannung' => '#2a8a52',
-            'Physiotherapie' => '#1a5080',
-            'Hilfsmittel' => '#7a4820',
-            'Alltag' => '#486070',
-            'Ernährung' => '#607020',
-            'Schlaf' => '#583878',
-            'Reha' => '#7a3060',
-        ])->each(function (string $color, string $name): void {
+            ['name' => 'Spastik',        'name_en' => 'Spasticity',     'color' => '#0a6e7a'],
+            ['name' => 'Muskeln',        'name_en' => 'Muscles',        'color' => '#b8762a'],
+            ['name' => 'Entspannung',    'name_en' => 'Relaxation',     'color' => '#2a8a52'],
+            ['name' => 'Physiotherapie', 'name_en' => 'Physiotherapy',  'color' => '#1a5080'],
+            ['name' => 'Hilfsmittel',    'name_en' => 'Assistive Aids', 'color' => '#7a4820'],
+            ['name' => 'Alltag',         'name_en' => 'Daily Life',     'color' => '#486070'],
+            ['name' => 'Ernährung',      'name_en' => 'Nutrition',      'color' => '#607020'],
+            ['name' => 'Schlaf',         'name_en' => 'Sleep',          'color' => '#583878'],
+            ['name' => 'Reha',           'name_en' => 'Rehabilitation', 'color' => '#7a3060'],
+        ])->each(function (array $tag): void {
             Tag::query()->updateOrCreate(
-                ['slug' => Str::slug($name)],
+                ['slug' => Str::slug($tag['name'])],
                 [
-                    'name' => $name,
-                    'color' => $color,
+                    'name' => $tag['name'],
+                    'name_en' => $tag['name_en'],
+                    'color' => $tag['color'],
                     'is_active' => true,
                 ],
             );
